cc423f11088c4bb54e0eead4bdb44cc0dc611043
Ross Keenan <ross.keenan@protonmail.com>
2021-06-18 18:34:44 -0500

    Added in useState. implemented button to increase count value onclick

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, { useState } from "react";
 
+// function expensiveInitialState(){
+//   return 10;
+// }
+
 function App() {
-  return <div className="App">Hey</div>;
+  //use arrow function with useState when you have an expensive function
+  //causes the component to rerender just once.
+  //const [count, setCount] = useState(() => expensiveInitialState());
+
+  const [count, setCount] = useState(10);
+
+  function incrementCount() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={incrementCount}>Click Me</button>
+      <div>{count}</div>
+    </div>
+  );
 }
 
 export default App;
