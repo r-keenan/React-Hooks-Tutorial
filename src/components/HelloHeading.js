9a039b5712c117c5d1a1753b0d8fe35f6db975c3
Ross Keenan <ross.keenan@protonmail.com>
2021-06-18 20:16:30 -0500

    Added in useEffect hook

---
+++ src/components/HelloHeading.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+export default function HelloHeading() {
+  useEffect(() => {
+    const onMouseMove = (e) => {
+      console.log(e);
+    };
+    window.addEventListener("mousemove", onMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+    };
+  }, []);
+
+  return <div>Hello! Please login with your credentials</div>;
+}
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import useForm from "../customHooks/useForm";
+import HelloHeading from "./HelloHeading";
 
 const containerStyle = {
   paddingTop: "30px",
 };
 
 export default function LoginForm() {
-  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: "",
+  });
+
+  const [showHello, setShowHello] = useState(true);
+
   return (
     <div style={containerStyle}>
+      <button onClick={() => setShowHello(!showHello)}>Toggle</button>
+      {showHello && <HelloHeading />}
       <div>
         <label>Email</label>
         <input name="email" value={values.email} onChange={handleChange} />
+      </div>
+      <div>
+        <label>First name</label>
+        <input
+          name="firstName"
+          value={values.firstName}
+          onChange={handleChange}
+          placeholder="First Name"
+        />
       </div>
       <div>
         <label>Password</label>
