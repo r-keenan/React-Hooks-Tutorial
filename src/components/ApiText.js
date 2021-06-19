84bb993780f34eb3995441f57d2a04bc5eb3abd7
Ross Keenan <ross.keenan@protonmail.com>
2021-06-19 08:10:12 -0500

    Added in persistent data using useEffect hook.

--- src/components/ApiText.js
+++ src/components/ApiText.js
@@ -1,6 +1,6 @@
 import useFetch from "../customHooks/useFetch";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const containerStyle = {
   marginTop: "20px",
@@ -8,8 +8,15 @@ const containerStyle = {
 };
 
 export default function ApiText() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count"))
+  );
   const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
   return (
     <div style={containerStyle}>
       <div>{!data ? "loading..." : data}</div>
